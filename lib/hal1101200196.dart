2a188b1edab9203e25f579e9b214e11fe1b43f59
lulubalqiszf <lulubalqisziankafazza@gmail.com>
2023-04-11 10:00:56 +0700

    balqis

--- lib/hal1101200196.dart
+++ lib/hal1101200196.dart
@@ -1,64 +1,493 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'dart:math';
 
-class hal1101200196 extends StatelessWidget {
-  // final String message;
+import 'package:flutter/material.dart';
+
+class hal1101200196 extends StatefulWidget {
+  // final String output;
   hal1101200196({super.key});
 
-  TextEditingController _textEditingController = TextEditingController();
+  @override
+  State<hal1101200196> createState() => _hal1101200196State();
+}
+
+class _hal1101200196State extends State<hal1101200196> {
+  final TextEditingController _textEditingController = TextEditingController();
+  String _output = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lulu Balqis Zianka Faza/Page-1'),
+      ),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Container(
+                height: 200,
+                width: 200,
+                decoration: const BoxDecoration(
+                  // color: Colors.red.withOpacity(0.1),
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://anaktelkom.com/wp-content/uploads/2021/08/Logo-Telkom-University-900x1024.png'),
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextField(
+                      controller: _textEditingController,
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.key),
+                        hintText: 'Ketikkan 7 digit pertama NIM anda',
+                      ),
+                    ),
+                    // Text('$output'),
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                              child: Text('Main Page'),
+                              onPressed: () {
+                                Navigator.pop(
+                                    context, _textEditingController.text);
+                              },
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                              child: Text('Next Page'),
+                              onPressed: () async {
+                                if (_textEditingController.text ==
+                                    1101200.toString()) {
+                                  final result = await Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => hal1101200196_2(
+                                          pesan1: _textEditingController.text),
+                                    ),
+                                  );
+                                  setState(() {
+                                    _output = result ?? '';
+                                  });
+                                }
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("NIM/No. Telp."),
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20.0), //or 15.0
+                child: Container(
+                  height: 70.0,
+                  width: 390.0,
+                  color: Color(0xffFF0E58),
+                  child: Center(
+                      child: Text(
+                    _output,
+                    style: TextStyle(fontSize: 28),
+                  )),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class hal1101200196_2 extends StatefulWidget {
+  // hal1101200196_2({super.key,required this.pesan1});
+  String pesan1 = '';
+  hal1101200196_2({super.key, required this.pesan1});
+  @override
+  _hal1101200196_2State createState() => _hal1101200196_2State();
+}
+
+class _hal1101200196_2State extends State<hal1101200196_2> {
+  String NIM7 = '';
+  TextEditingController _textEditingController0 = TextEditingController();
+  TextEditingController _textEditingController1 = TextEditingController();
+  TextEditingController _textEditingController2 = TextEditingController();
+  String _output = '';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Balqis'),
+        title: const Text('Lulu Balqis Zianka Faza/Page-2'),
       ),
-      body: Column(
+      body: ListView(
         children: [
-          Container(
-            height: 200,
-            width: 200,
-            decoration: const BoxDecoration(
-              // color: Colors.red.withOpacity(0.1),
-              image: DecorationImage(
-                image: NetworkImage(
-                    'https://anaktelkom.com/wp-content/uploads/2021/08/Logo-Telkom-University-900x1024.png'),
-                fit: BoxFit.fitHeight,
-              ),
-            ),
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextField(
-                  controller: _textEditingController,
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.key),
-                    hintText: 'Masukkan 5 digit pertama NIM anda',
-                  ),
-                ),
-                // Text('$message'),
-                Row(
+          //Silakan lanjutkan........................
+          Column(
+            children: [
+              Center(
+                child: Column(
                   children: [
-                    ElevatedButton(
-                      child: Text('Go back to First Page'),
-                      onPressed: () {
-                        Navigator.pop(context, _textEditingController.text);
-                      },
+                    SizedBox(
+                      height: 20,
+                      width: 20,
                     ),
-                    ElevatedButton(
-                      child: Text('Next Page'),
-                      onPressed: () {
-                        Navigator.pop(context, _textEditingController.text);
-                      },
+                    TextField(
+                      controller: _textEditingController0,
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.key),
+                        hintText: 'Ketikkan Alamat Email Anda',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                      width: 20,
+                    ),
+                    TextField(
+                      controller: _textEditingController1,
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.key),
+                        hintText: 'Ketikkan No handphone Anda',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                      width: 20,
+                    ),
+                    TextField(
+                      controller: _textEditingController2,
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.key),
+                        hintText: 'Ketikkan digit ke-8 NIM Anda',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                      width: 20,
+                    ),
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                              child: Text('Previous Page'),
+                              onPressed: () {
+                                Navigator.pop(context, _output);
+                              },
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                              child: Text('Next Page'),
+                              onPressed: () async {
+                                if (_textEditingController2.text ==
+                                    1.toString()) {
+                                  String NIM7 = '${widget.pesan1}' +
+                                      _textEditingController2.text;
+                                  final result = await Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => hal1101200196_3(
+                                        pesan1: NIM7,
+                                        pesan2: _textEditingController0.text,
+                                        pesan3: _textEditingController1.text,
+                                      ),
+                                    ),
+                                  );
+                                  setState(() {
+                                    _output = result ?? '';
+                                  });
+                                  ;
+                                }
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("NIM/No. Telp."),
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20.0), //or 15.0
+                child: Container(
+                  height: 70.0,
+                  width: 390.0,
+                  color: Color.fromARGB(0, 7, 252, 117),
+                  child: Center(
+                      child: Text(
+                    _output,
+                    style: TextStyle(fontSize: 28),
+                  )),
+                ),
+              ),
+            ],
           ),
         ],
+      ),
+    );
+  }
+}
+
+class hal1101200196_3 extends StatefulWidget {
+  String pesan1 = '';
+  String pesan2 = '';
+  String pesan3 = '';
+  hal1101200196_3(
+      {super.key,
+      required this.pesan1,
+      required this.pesan2,
+      required this.pesan3});
+  @override
+  _hal1101200196_3State createState() => _hal1101200196_3State();
+}
+
+class _hal1101200196_3State extends State<hal1101200196_3> {
+  String NIM10 = '';
+  String TLP = '';
+  TextEditingController _textEditingController3 = TextEditingController();
+  String _output = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lulu Balqis Zianka Faza/Page-3'),
+      ),
+      body: ListView(
+        children: [
+          //Silakan lanjutkan........................
+          Column(
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: AssetImage('lib/images/balqis.jpeg'),
+                      radius: 100,
+                    ),
+                    SizedBox(
+                      height: 20,
+                      width: 20,
+                    ),
+                    Text(
+                      '${widget.pesan2}',
+                      style: TextStyle(fontSize: 22),
+                    ),
+                    SizedBox(
+                      height: 20,
+                      width: 20,
+                    ),
+                    Text(
+                      '${widget.pesan3}',
+                      style: TextStyle(fontSize: 22),
+                    ),
+                    SizedBox(
+                      height: 20,
+                      width: 20,
+                    ),
+                    TextField(
+                      controller: _textEditingController3,
+                      decoration: InputDecoration(
+                        icon: Icon(Icons.key),
+                        hintText: 'Masukkan digit ke-9 dan ke-10 NIM anda',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                      width: 20,
+                    ),
+                    Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                              child: Text('Previous Page'),
+                              onPressed: () {
+                                Navigator.pop(context, _output);
+                              },
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ElevatedButton(
+                              child: Text('Next Page'),
+                              onPressed: () async {
+                                if (_textEditingController3.text ==
+                                    96.toString()) {
+                                  String NIM10 = '${widget.pesan1}' +
+                                      _textEditingController3.text;
+                                  String TLP = '${widget.pesan3}';
+                                  final result = await Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => hal1101200196_4(
+                                        pesan1: NIM10,
+                                        pesan2: TLP,
+                                        pesan3: _textEditingController3.text,
+                                      ),
+                                    ),
+                                  );
+                                  setState(() {
+                                    _output = result ?? '';
+                                  });
+                                }
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("NIM/No. Telp."),
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(20.0), //or 15.0
+                child: Container(
+                  height: 70.0,
+                  width: 390.0,
+                  color: Color.fromARGB(0, 7, 252, 117),
+                  child: Center(
+                      child: Text(
+                    _output,
+                    style: TextStyle(fontSize: 28),
+                  )),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class hal1101200196_4 extends StatefulWidget {
+  // hal1101200196_2({super.key,required this.pesan1});
+  String pesan1 = '';
+  String pesan2 = '';
+  String pesan3 = '';
+  hal1101200196_4(
+      {super.key,
+      required this.pesan1,
+      required this.pesan2,
+      required this.pesan3});
+  @override
+  _hal1101200196_4State createState() => _hal1101200196_4State();
+}
+
+class _hal1101200196_4State extends State<hal1101200196_4> {
+  //
+  @override
+  Widget build(BuildContext context) {
+    String allPesan = '${widget.pesan1}/${widget.pesan2}';
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lulu Balqis Zianka Faza/Page-4'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 20,
+                    width: 20,
+                  ),
+                  Text(
+                    '${widget.pesan1} / ${widget.pesan2}',
+                    style: TextStyle(fontSize: 22),
+                  ),
+                  SizedBox(
+                    height: 20,
+                    width: 20,
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                          child: Text('Previous Page'),
+                          onPressed: () {
+                            Navigator.pop(context, allPesan);
+                          }),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: GridView(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 1),
+                children: [
+                  GridView.count(
+                    crossAxisCount: 5,
+                    scrollDirection: Axis.vertical,
+                    padding: EdgeInsets.all(0),
+                    children: List.generate(allPesan.length, (index) {
+                      return Container(
+                        height: 1,
+                        child: Card(
+                          color: Colors.primaries[
+                              Random().nextInt(Colors.primaries.length)],
+                          child: Center(
+                            child: Text(
+                              allPesan[index],
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
