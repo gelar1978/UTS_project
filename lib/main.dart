b1dc79607615da0a7eafadda7de1bde14b21c7ed
sabilahj27 <sabilahj@gmail.com>
2023-05-08 17:51:04 +0700

    Sabila

--- lib/main.dart
+++ lib/main.dart
@@ -259,7 +259,7 @@ class _Hal1State extends State<Hal1> {
                   final result = await Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => hal1101204132(),
+                      builder: (context) => hal1101204132old(),
                     ),
                   );
                   setState(() {
