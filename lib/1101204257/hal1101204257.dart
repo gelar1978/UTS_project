caf1269b1598f3f54aa72aa33284e6f8c29bca98
rifqifadhillahf71 <rifqifadhilahfirdaus@student.telkomuniversity.ac.id>
2023-05-09 06:50:51 +0700

    Rifqi
    update

---
+++ lib/1101204257/hal1101204257.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/1101204257/hal1101204257.dart';
+import 'package:flutter_application_1/1101204257/reset1101204257.dart';
+import 'package:flutter_application_1/1101204257/signup1101204257.dart';
+import 'package:sign_button/sign_button.dart';
+// import 'package:flutter_signin_button/flutter_signin_button.dart';
+// import 'package:sign_button/sign_button.dart'
+// void main() => runApp(const MyApp());
+
+class hal1101204257new extends StatelessWidget {
+  const hal1101204257new({Key? key}) : super(key: key);
+
+  static const String _title = 'LOGIN PAGE';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  String _message = '';
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(1),
+                child: const Text(
+                  'Gelar Budiman',
+                  style: TextStyle(
+                      color: Colors.red,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20),
+                )),
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(1),
+                child: const Text(
+                  '1101204257',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 20),
+                )),
+            CircleAvatar(
+              radius: 60,
+              child: Container(
+                height: 120,
+                width: 120,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("lib/images/glb.jpg"),
+                    fit: BoxFit.fitWidth,
+                  ),
+                  shape: BoxShape.circle,
+                ),
+              ),
+            ),
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Sign in',
+                  style: TextStyle(fontSize: 20),
+                )),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'User Name',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => reset1101204257(),
+                  ),
+                );
+                //forgot password screen
+              },
+              child: const Text(
+                'Forgot Password',
+              ),
+            ),
+            Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: ElevatedButton(
+                  child: const Text('Login'),
+                  onPressed: () async {
+                    final result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => hal1101204257old(),
+                      ),
+                    );
+                    setState(() {
+                      _message = result ?? '';
+                    });
+                  },
+                  // onPressed: () {
+                  //   print(nameController.text);
+                  //   print(passwordController.text);
+                  // },
+                )),
+            // SignInButton(
+            //   btnText: 'Login',
+            //   buttonSize: ButtonSize.small,
+            //   onPressed: () {},
+            //   buttonType: ButtonType.microsoft,
+            // ),
+            SignInButton(
+              buttonSize: ButtonSize.small,
+              onPressed: () {},
+              buttonType: ButtonType.google,
+            ),
+            SignInButton(
+              // shape: ,
+              buttonSize: ButtonSize.small,
+              onPressed: () {},
+              buttonType: ButtonType.facebook,
+            ),
+            Row(
+              children: <Widget>[
+                const Text('Does not have account?'),
+                TextButton(
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SignUpScreen1101204257(),
+                      ),
+                    );
+                    //signup screen
+                  },
+                )
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+          ],
+        ));
+  }
+}
