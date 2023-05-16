05186f8710a8da85f1a493d05fe497054110dadd
dewinachdalina <dewi.n.achdalina@gmail.com>
2023-05-16 07:44:28 +0700

    .

---
+++ lib/1101204257/home1101204257.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_application_1/services/auth_service.dart';
+
+class home1101204257 extends StatefulWidget {
+  // final String message;
+  home1101204257({super.key});
+
+  @override
+  State<home1101204257> createState() => _home1101204257State();
+}
+
+class _home1101204257State extends State<home1101204257> {
+  final TextEditingController _textEditingController = TextEditingController();
+  String _message = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HOME'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              // _showAlertDialog(context);
+              AuthService.signOut();
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Center(
+          child: ListView(
+            children: <Widget>[
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(1),
+                  child: const Text(
+                    'Rifqi Fadhilah Firdaus',
+                    style: TextStyle(
+                        color: Colors.red,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20),
+                  )),
+              Container(
+                  alignment: Alignment.center,
+                  padding: const EdgeInsets.all(1),
+                  child: const Text(
+                    '1101204257',
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20),
+                  )),
+              CircleAvatar(
+                radius: 60,
+                child: Container(
+                  height: 120,
+                  width: 120,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("lib/images/glb.jpg"),
+                      fit: BoxFit.fitWidth,
+                    ),
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
