4017f572f65c41d19f060bb18543f03bb486df7b
lulubalqiszf <lulubalqisziankafazza@gmail.com>
2023-05-23 08:00:22 +0700

    balqis

---
+++ lib/1101200196/viewdata1101200196.dart
@@ -0,0 +1,63 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+import 'user1101200196.dart' as data1101200196;
+
+class View1101200196 extends StatefulWidget {
+  const View1101200196({super.key});
+  @override
+  State<View1101200196> createState() => _View1101200196State();
+}
+
+class _View1101200196State extends State<View1101200196> {
+// data[]
+  String _message = '';
+
+// Map<int, String> Nama=Data.
+  // TextEditingController _textEditingController0 = TextEditingController();
+  List<int> NIM = data1101200196.index.values.toList();
+  List<String> Nama = data1101200196.nama.values.toList();
+  // @overrides
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('List View with CircleAvatar'),
+        ),
+        body: ListView.builder(
+          itemCount: NIM.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              // onTap: () async {
+              //   final result = await Navigator.push(
+              //     context,
+              //     MaterialPageRoute(
+              //       builder: (context) => hal1101200196new(),
+              //     ),
+              //   );
+              //   setState(() {
+              //     _message = result ?? '';
+              //   });
+              // },
+              trailing: CircleAvatar(
+                child: Text(Nama[index]),
+                // backgroundColor: Colors,
+                // backgroundImage: NetworkImage(Nama[index]),
+              ),
+              leading: CircleAvatar(
+                child: Text(NIM[index].toString()),
+                // backgroundColor: Colors,
+                // backgroundImage: NetworkImage(Nama[index]),
+              ),
+              title: Text(Nama[index]),
+              subtitle: Text(NIM[index].toString()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
